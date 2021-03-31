483c8d598f354c6da66f86daac7c65fb171e4940
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-03-31 17:47:24 +0200

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -4,7 +4,7 @@ const config = require('../config.json');
 module.exports = {
   name: "orderdelete",
   aliases: ["delorders", "del"],
-  description: "Delete server's orders",
+  description: "Supprimer les commandes du serveur",
   execute: async(client, message, args, data, db) => {
     let onwerId = config.OwnerID;
 
@@ -16,11 +16,11 @@ module.exports = {
     db.delete(`code_${serverid}`)
     db.delete(`description_${serverid}`) 
     db.delete(`orders_${serverid}`)
-    console.log(`the order of guild ${serverid} has been deleted!`);
-    message.channel.send(`the order of guild **${client.guilds.cache.get(serverid).name}** has been deleted!`);
+    console.log(`the order of guild ${serverid} a été supprimé!`);
+    message.channel.send(`the order of guild **${client.guilds.cache.get(serverid).name}** a été supprimé!`);
     let channelll = client.channels.cache.get(config.logChannel)
     let embed = new Discord.MessageEmbed()
-    .setTitle(`Server Order Deleted!`)
+    .setTitle(`Commande de serveur supprimée!`)
     .setDescription(`<@${message.author.id}> Deleted **${client.guilds.cache.get(serverid).name}** Orders!`)
     channelll.send(embed)
 
