b7836389acb58fb79415e60cb55e645a2169b9b0
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-03-31 17:21:27 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setAuthor(client.user.username, client.user.displayAvatarURL())
     .setThumbnail(client.user.avatarURL())
-    .addField('INVITE THE BOT TO YOUR SERVER ',`Invite the bot using this link: https://discord.com/oauth2/authorize?client_id=804993556749353013&permissions=93185&scope=bot`)    
+    .addField('INVITE THE BOT TO YOUR SERVER ',`Invite the bot using this link: https://discordapp.com/oauth2/authorize?client_id=826780262884966460&scope=bot&permissions=2146958847`)    
     .setFooter(config.EmbedFooter)
     .setColor("#8a8aff")
     .setImage('https://i.gyazo.com/a65915aa5e7aa181d34d85bea1818be4.png')
