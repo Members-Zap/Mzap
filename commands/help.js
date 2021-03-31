a7ecf18fb22c2dfbc0517d2609c6453349a0b0d1
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-03-31 17:26:54 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,29 +3,29 @@ const config = require('../config.json');
 module.exports = {
   name: "help",
   aliases: ["help"],
-  description: "displays the bot commands list.",
+  description: "affiche les commandes du bot list.",
   execute: async(client, message, args, data, db) => {
      const embed = new Discord.MessageEmbed()
     .setColor(`RANDOM`)
     .setAuthor(client.user.username, client.user.displayAvatarURL())
     .setTitle(`${client.user.username} v2.0`)
     .setThumbnail(client.user.displayAvatarURL())
-    .setDescription(`If you need more help, feel free to ask our support team in the server ${config.supportServer}`)
+    .setDescription(`Si vous avez besoin de plus d'aide, n'hésitez pas à demander à notre équipe d'assistance sur le serveur ${config.supportServer}`)
     .addFields(
-      { name: `+help`, value: `Shows the help command. [invite Me]()`, inline: true},
-      { name: `+invite`, value: `Invite the bot to your server. [invite Me]()`, inline: true},
-      { name: `+bal | +balance`, value: `Check your coins balance. [invite Me]()`, inline: true },
-      { name: `+f | +find`, value: `Find some servers to join for coins. [invite Me]()`, inline: true },
-      { name: `+pay`, value: `Pay some coins to other. [invite Me]()`, inline: true},
-      { name: `+check`, value: `Check where you can leave servers. [invite Me]()`, inline: true},
-      { name: `+order | +info`, value: `See your order information. [invite Me]()`, inline: true},
-      { name: `+buy`, value: `To buy users in your server. [invite Me]()`, inline: true} ,
-      { name: `+vote`, value: `Vote and get 1 coin. [invite Me]()`, inline: true},
-      { name: `+giftcode`, value: `To convert your coins into gift code. [invite Me]()`, inline: true},
-      { name: `+botinfo`, value: `It gives you the bot information. [invite Me]()`, inline: true},
-      { name: `+stats`, value: `It gives you the detailed stats of bot. [invite Me]()`, inline: true})
+      { name: `.help`, value: `Affiche la commande d'aide. [invite Me]()`, inline: true},
+      { name: `.invite`, value: `Invitez le bot sur votre serveur. [invite Me]()`, inline: true},
+      { name: `.bal | .balance`, value: `Vérifiez le solde de vos pièces. [invite Me]()`, inline: true },
+      { name: `.f | .find`, value: `Find some servers to join for coins. [invite Me]()`, inline: true },
+      { name: `.pay`, value: `Payer des pièces à d'autres. [invite Me]()`, inline: true},
+      { name: `.check`, value: `Vérifiez où vous pouvez quitter les serveurs. [invite Me]()`, inline: true},
+      { name: `.order | +info`, value: `Voir les informations de votre commande. [invite Me]()`, inline: true},
+      { name: `.buy`, value: `Pour acheter des utilisateurs sur votre serveur. [invite Me]()`, inline: true} ,
+      { name: `.vote`, value: `Votez et obtenez 1 pièce. [invite Me]()`, inline: true},
+      { name: `.giftcode`, value: `Pour convertir vos pièces en code cadeau. [invite Me]()`, inline: true},
+      { name: `.botinfo`, value: `Il vous donne les informations sur le bot. [invite Me]()`, inline: true},
+      { name: `.stats`, value: `Il vous donne les statistiques détaillées du bot. [invite Me]()`, inline: true})
   
    
-    message.channel.send(embed).catch(e => message.channel.send(`I don't have permission to send embed message here!`)) 
+    message.channel.send(embed).catch(e => message.channel.send(`Je ne suis pas autorisé à envoyer un message intégré ici!`)) 
   }
 }
