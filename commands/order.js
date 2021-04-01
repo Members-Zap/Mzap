719f0c5dca6a864a79fceb00a1f33f2d13d6eacb
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 17:19:30 +0200

    Update order.js

--- commands/order.js
+++ commands/order.js
@@ -3,10 +3,10 @@ const config = require('../config.json');
 module.exports = {
   name: "order",
   aliases: ["info", "orders"],
-  description: "shows the current order of the guild.",
+  description: "montre l'ordre actuel de la guilde.",
   execute: async(client, message, args, data, db) => {
    
-    if (data.code == 0) return message.channel.send(`There isn't any order in this guild.`)
+    if (data.code == 0) return message.channel.send(`Il n'y a pas d'ordre dans cette guilde.`)
     
     let bar = []
     
@@ -20,15 +20,15 @@ module.exports = {
     
     let warn = ""
     
-    await client.fetchInvite('https://discord.gg/' + data.code).catch(e => warn = "The invite link of this guild is expired! Please make a new order or no one will be able to join here!")
+    await client.fetchInvite('https://discord.gg/' + data.code).catch(e => warn = "Le lien d'invitation de cette guilde est expiré! Veuillez passer une nouvelle commande ou personne ne pourra se joindre ici!")
     
    const embed = new Discord.MessageEmbed()
     .setColor("#8a8aff")
     .setAuthor(client.user.username, client.user.displayAvatarURL())
     .setTitle(`${message.guild.name} order info:`)
     .setThumbnail(message.channel.guild.iconURL())
-    .addField(`Want Faster Orders Completion?`, ` You will get faster orders completion!`)
-    .setDescription(`Total orders: ${data.orders}\nProgress: ${bar.join("")} ${data.uses}/${data.orders}`)
+    .addField(`Want Faster Orders Completion?`, ` Vous obtiendrez une exécution plus rapide des commandes!`)
+    .setDescription(`Total ordres: ${data.orders}\nProgress: ${bar.join("")} ${data.uses}/${data.orders}`)
     .setFooter(config.EmbedFooter)
 
     message.channel.send(warn, embed) 
