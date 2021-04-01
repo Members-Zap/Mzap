d7361c66fdc0a9dab45fa9a0c5a404597ad01308
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 19:15:29 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 module.exports = {
   name: "unban",
-  description: "supprime un utilisateur du bot, propriétaire uniquement.",
+  description: "unbans an user from the bot, owner only.",
   execute: async(client, message, args, data, db) => {
 
     let owners = config.OwnerID;
@@ -11,18 +11,18 @@ module.exports = {
 
     let user = client.users.cache.find(user => args.length && message.mentions.users.size < 1 && user.username.toLowerCase().startsWith(args.join(" ").toLowerCase())) || client.users.cache.get(args[0]) || message.mentions.users.first()
 
-    if (user === undefined) return message.channel.send(`**Commande incorrecte** ||Noob Propriétaire||\nUsage: \`${config.prefix}ban [@user/username/userID]\``)
+    if (user === undefined) return message.channel.send(`**Incorrect Command** ||Noob Owner||\nUsage: \`${config.prefix}ban [@user/username/userID]\``)
 
     let embedd = new Discord.MessageEmbed()
-    .setTitle('Vous n'êtes pas banni!')
-    .setDescription(`<@${message.author.id}> vous interdire! Congo!!`)
+    .setTitle('You are Unbanned!')
+    .setDescription(`<@${message.author.id}> unbanned you! Congo!!`)
     .setThumbnail(user.displayAvatarURL({ format: 'png', dynamic: true }))
     .setFooter(config.EmbedFooter, user.displayAvatarURL({ format: 'png', dynamic: true }))
 
 
     if (await db.fetch(`banned_${user.id}`) == true) 
 	user.send(embedd)
-	message.channel.send('Non banni!')
+	message.channel.send('Unbanned!')
 
 
 
@@ -34,7 +34,7 @@ module.exports = {
     let unbannedchannel = client.channels.cache.get(config.logChannel)
     let can = new Discord.MessageEmbed()
     .setTitle('User Unbanned!')
-    .setDescription(`User Nom: **${user.username}**\nUser ID: **${user.id}**\nUnbanned by: <@${message.author.id}>`)
+    .setDescription(`User Name: **${user.username}**\nUser ID: **${user.id}**\nUnbanned by: <@${message.author.id}>`)
     .setFooter(config.EmbedFooter, user.displayAvatarURL({ format: 'png', dynamic: true }))
     .setColor('BLUE')
     .setThumbnail(user.displayAvatarURL({ format: 'png', dynamic: true }))
@@ -43,3 +43,4 @@ module.exports = {
     
   }
 }
+
