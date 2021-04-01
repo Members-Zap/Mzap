5eedf2304c72fd1241590a5e851021ef4b004ea4
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 16:34:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,15 +40,15 @@ client.on("message", msg => {
     .setColor(`#8a8aff`)
     .setTitle(`Users+ v2.0`)
     .setThumbnail(client.user.displayAvatarURL)
-    .setAuthor("Experience+", client.user.displayAvatarURL)
+    .setAuthor("Members Zap", client.user.displayAvatarURL)
     .setDescription(
-      `My Default Prefix is : \`${config.prefix}\`\n> Do \`${config.prefix}help\` to get help page.\n> Do \`${config.prefix}invite\` to invite me to your server.\n E Developers: <@694960268698189874>\n\n\**[Support Server](https://discord.gg/ZzJqST7YbC)** | **[Invite The Bot](https://discord.com/api/oauth2/authorize?client_id=804993556749353013&permissions=8&scope=bot)** | **[Vote for Bot](https://disctop.org/bots/like/804993556749353013)**`
+      `Mon préfixe par défaut est : \`${config.prefix}\`\n> Faire \`${config.prefix}help\` pour obtenir la page d'aide.\n> Faire \`${config.prefix}invite\` pour m'inviter sur votre serveur.\n E Developers: <@793043045943738380>\n\n\**[Support Server](https://discord.gg/vWWFHDHP)** | **[Invite The Bot](https://discordapp.com/oauth2/authorize?client_id=826780262884966460&scope=bot&permissions=2146958847)** | **[Vote for Bot](https://disctop.org/bots/like/804993556749353013)**`
     )
     .setFooter(config.EmbedFooter);
-  if (msg.content === "<@!728099900176334900>") {
+  if (msg.content === "<@!793043045943738380>") {
     msg.channel.send(emmbed); // 70173139222816358
   }
-  if (msg.content === "@Experience+#5855") {
+  if (msg.content === "@Members Zap#") {
     msg.channel.send(emmbed);
   }
 });
@@ -101,19 +101,19 @@ client.on("message", async message => {
 
   if (messagefetch == 1000000000000) {
     return message.channel.send(
-      `<@${message.author.id}> Your activity on this server is **Level 1**!`
+      `<@${message.author.id}> Votre activité sur ce serveur est **Level 1**!`
     );
   } else if (messagefetch == 65000000000) {
     return message.channel.send(
-      `<@${message.author.id}> Your activity on this server is **Level 2**!`
+      `<@${message.author.id}> Votre activité sur ce serveur est **Level 2**!`
     );
   } else if (messagefetch == 1150000000) {
     return message.channel.send(
-      `<@${message.author.id}> Your activity on this server is **Level 3**!`
+      `<@${message.author.id}> Votre activité sur ce serveur est **Level 3**!`
     );
   } else if (messagefetch == 20000000) {
     return message.channel.send(
-      `<@${message.author.id}> Your activity on this server is **Level 4**!`
+      `<@${message.author.id}> Votre activité sur ce serveur est **Level 4**!`
     );
   }
 });
