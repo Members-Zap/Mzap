80fb732337a3cc1a4ef847be2d6b8b96f3f4015f
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 17:22:49 +0200

    Update subtract.js

--- commands/subtract.js
+++ commands/subtract.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 module.exports = {
   name: "remove",
   aliases: ["coinsremove", "removecoins"],
-  description: "removed coins from a user, owner only.",
+  description: "a supprimé les pièces d'un utilisateur, propriétaire uniquement.",
   execute: async(client, message, args, data, db) => {
 
     let owners = config.OwnerID;
@@ -11,13 +11,13 @@ module.exports = {
     if (!owners.includes(message.author.id)) return;
 
     let pay = Number(args[1])
-    if (!pay || isNaN(pay)) return message.channel.send(`**Incorrect Command!** ||Noob Owner||\nTry: *${config.prefix}remove <User> <coins>*`)
+    if (!pay || isNaN(pay)) return message.channel.send(`**Commande incorrecte!** ||Propriétaire Noob||\nTry: *${config.prefix}remove <User> <coins>*`)
 
     let user = client.users.cache.find(user => args.length && message.mentions.users.size < 1 && user.username.toLowerCase().startsWith(args.slice(0, user.username.split(" ").length).join(" ").toLowerCase())) || client.users.cache.get(args[0]) || message.mentions.users.first()
 
     let subembed = new Discord.MessageEmbed()
-    .setTitle(`Coins Removed!`)
-    .setDescription(`<@${message.author.id}> Removed your **${pay}** coins!`)
+    .setTitle(`Coins Supprimé!`)
+    .setDescription(`<@${message.author.id}> Supprimé votre **${pay}** coins!`)
     .setThumbnail(message.author.displayAvatarURL({ format: "png", dynamic: true }))
     .setFooter(config.EmbedFooter, message.author.displayAvatarURL({ format: "png", dynamic: true }))
     .setColor(`#fca903`)
@@ -27,8 +27,8 @@ module.exports = {
 /*
     let logchannel = client.channels.cache.get("745349291756748832")
     let embed1 = new Discord.MessageEmbed()
-    .setTitle('Removed Coins!')
-    .setDescription(`User Name: **${user.username}**\nUser Id: **${user.id}**\nCoins Removed: **${pay}**\nRemoved by: <@${message.author.id}>`)
+    .setTitle('Supprimé Coins!')
+    .setDescription(`User Nom: **${user.username}**\nUser Id: **${user.id}**\nCoins Supprimé: **${pay}**\nSupprimé par: <@${message.author.id}>`)
     .setFooter(config.EmbedFooter, user.displayAvatarURL({ format: "png", dynamic: true }))
     .setColor("#fca903")
     .setThumbnail(user.displayAvatarURL({ format: "png", dynamic: true }))
