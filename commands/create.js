987de2cfb40b123663069fa5c173d25230edf141
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 17:10:40 +0200

    Update create.js

--- commands/create.js
+++ commands/create.js
@@ -9,16 +9,16 @@ module.exports = {
       if(args[0] == 'create') {
 
         let COINS = Number(args[1])
-        if (!COINS || isNaN(COINS) || COINS < 1) return message.channel.send(`Commanding incorrect!\nIn order to buy giftcard, do \`-giftcode create <numberOfCoins> <Code>\``)
-        if (COINS > data.coins) return message.channel.send(`${message.author.username} you don't have enough Balance.\n\n\`If you Are Getting this ! please Try joining Some servers First then Try this command again\``)
+        if (!COINS || isNaN(COINS) || COINS < 1) return message.channel.send(`Commande incorrecte!\nIn pour acheter une carte-cadeau, faites \`-giftcode create <numberOfCoins> <Code>\``)
+        if (COINS > data.coins) return message.channel.send(`${message.author.username} tu n'as pas assez d'équilibre.\n\n\`Si vous obtenez ceci! Veuillez essayer de rejoindre certains serveurs d'abord, puis réessayez cette commande\``)
         COINS = Math.round(COINS)
 
       let CODE = args[2]
-      message.reply(`Check dm!`)
+      message.reply(`Vérifier dm!`)
       message.delete()
       let embed = new Discord.MessageEmbed()
-      .setTitle(`Gift Code Generated!`)
-      .setDescription(`<@${message.author.id}> You have Converted your coins into Gift Code!`)
+      .setTitle(`Code cadeau généré!`)
+      .setDescription(`<@${message.author.id}> Vous avez converti vos pièces en code cadeau!`)
       .addField(`Code:`, `${CODE}`, false)
       .addField(`Total Coins:`, `${COINS}`, false)
       .setThumbnail(message.author.displayAvatarURL({ format: "png", dynamic: true }))
@@ -29,7 +29,7 @@ module.exports = {
       await new Promise(resolve => setTimeout(resolve, 100))
       db.add(`code_` + CODE, COINS)
       db.subtract(`coins_${message.author.id}`, Number(COINS))
-      data.logs.unshift(`[-${COINS}] - Converted Coins into Gift Code.`)
+      data.logs.unshift(`[-${COINS}] - Pièces converties en code cadeau.`)
 
       db.set(`logs_${message.author.id}`, data.logs)
 
@@ -37,13 +37,13 @@ module.exports = {
         REDEEMINGCODE = args[1]
         let COINSTOADD = db.fetch(`code_` + REDEEMINGCODE)
         if (COINSTOADD == null || COINSTOADD == 0) {
-              message.channel.send('This code has either been redeemed already or is invalid!')
+              message.channel.send('Ce code a déjà été utilisé ou n'est pas valide!')
             } else {
                 const redeemed = new Discord.MessageEmbed()
                 .setColor("#8a8aff")
-                .setTitle(`CODE HAS BEEN REDEEMED SUCESSFULLY!`)
+                .setTitle(`LE CODE A ÉTÉ RÉCOMPENSÉ AVEC SUCCÈS!`)
                 .setFooter(config.EmbedFooter)
-                .setDescription(`➡ **You redeemed \`${COINSTOADD}\` coins from the Gift code!**`)
+                .setDescription(`➡ **Vous avez racheté \`${COINSTOADD}\` coins du code cadeau!**`)
                 message.channel.send(redeemed)
           data.logs.unshift(`[+${COINSTOADD}] - Redeemed a Gift code.`)
           db.set(`logs_${message.author.id}`, data.logs)
@@ -53,8 +53,8 @@ module.exports = {
         } else {
           let helpembed = new Discord.MessageEmbed()
           .setTitle('Gift Code Command Help!')
-          .addField(`\`+giftcode create <Coins> <Code>\``, `Using this you can convert your coins into Gift codes!`, false)
-          .addField(`\`+giftcode redeem <Code>\``, `Using this you can redeem Gift Codes!`, false)
+          .addField(`\`.giftcode create <Coins> <Code>\``, `En utilisant cela, vous pouvez convertir vos pièces en codes cadeaux!`, false)
+          .addField(`\`.giftcode redeem <Code>\``, `En utilisant cela, vous pouvez utiliser des codes cadeaux!`, false)
           .setFooter(config.EmbedFooter, message.author.displayAvatarURL({ format: "png", dynamic: true }))
           .setColor('YELLOW')
         message.channel.send(helpembed)
