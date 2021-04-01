386b990b1f2f02b73838135a6f7778e772e81e30
Jules-Z-YT <81090876+Jules-Z-YT@users.noreply.github.com>
2021-04-01 17:44:35 +0200

    Update premiuminfo.js

--- commands/premiuminfo.js
+++ commands/premiuminfo.js
@@ -4,7 +4,7 @@ const config = require("../config.json");
 module.exports = {
   name: "premiuminfo",
   aliases: ["infopremium", "help premium"],
-  description: "shows the bot premium information ",
+  description: "affiche les informations sur la prime du bot ",
   execute: async (client, message, args, data, db) => {      
   
      const embed = new Discord.MessageEmbed()
@@ -13,11 +13,11 @@ module.exports = {
     .setTitle(`${client.user.username}`)
     .setThumbnail(client.user.displayAvatarURL())
     .setDescription(`If you need more help, feel free to ask our support team in the server https://discord.gg/ZzJqST7YbC`)
-    .addField(`-THE PREMIUM WILL ALLOW YOU TO ACESS GIFTCODE COMMANDS
+    .addField(`-LA PREMIUM VOUS PERMETTRA D'ACCÉDER AUX COMMANDES DU CODE CADEAU
 
--1): YOU CAN CREATE COINS GIFTCODE 
--2): YOU CAN REDEEM COINS GIFTCODE 
--IF YOU DONT KNOW WHAT IS GIFTCODE DO +giftcode`, false);
+-1): VOUS POUVEZ CRÉER DES PIÈCES CODE CADEAU 
+-2): VOUS POUVEZ ÉCHANGER DES PIÈCES 
+-IF VOUS NE SAVEZ PAS QU'EST-CE QUE LE CODE CADEAU FAIT .giftcode`, false);
  message.channel.send(embed);
   }
 };
